63afc8718586ba5564974584f86bde6e11fb1139
RyanVohra <87702860+RyanVohra@users.noreply.github.com>
2021-08-06 16:20:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var S1
+
+
+
+
+function setup() {
+
+  createCanvas(400,400);
+ S1 = new Student("ryan",14,8);
+}
+
+function draw() 
+{
+  background(30);
+  S1.show();
+}
---
+++ student.js
@@ -0,0 +1,18 @@
+
+class Student
+{
+   constructor(n,a,g){
+  this.n=n;
+  this.a=a;
+  this.g=g;
+
+   }
+    show(){
+   console.log(this.n)
+   console.log(this.a)
+   console.log(this.g)
+    }
+}
+
+
+
